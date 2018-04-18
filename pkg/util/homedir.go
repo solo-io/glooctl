f063ad439cb9cd12f84befa081a8a3858c9102d9
Ashish Shrestha <axhixh@gmail.com>
2018-04-18 16:34:47 -0400

    load/save storage config

--- pkg/util/homedir.go
+++ pkg/util/homedir.go
@@ -1,10 +1,29 @@
 package util
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
 
 func HomeDir() string {
 	if h := os.Getenv("HOME"); h != "" {
 		return h
 	}
 	return os.Getenv("USERPROFILE") // windows
+}
+
+func ConfigDir() (string, error) {
+	d := filepath.Join(HomeDir(), ".glooctl")
+	_, err := os.Stat(d)
+	if err == nil {
+		return d, nil
+	}
+	if os.IsNotExist(err) {
+		if err := os.Mkdir(d, 0755); err != nil {
+			return "", err
+		}
+		return d, nil
+	}
+
+	return d, err
 }
