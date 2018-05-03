fce451222504c236d513473ab6b5d2101165dfc2
Ashish Shrestha <axhixh@gmail.com>
2018-05-03 16:29:36 -0400

    update route can now update more than just extensions

--- cmd/route/update.go
+++ cmd/route/update.go
@@ -18,6 +18,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	// represents the new route defintion for update
+	oldRouteOpt = &routeOption{route: &routeDetail{kube: &kubeUpstream{}}}
+)
+
 func updateCmd(opts *bootstrap.Options) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "update",
@@ -46,7 +51,31 @@ matcher and destination only. It doesn't include extensions.`,
 	flags.BoolVar(&routeOpt.sort, "sort", false, "sort the routes after appending the new route")
 	flags.BoolVarP(&routeOpt.interactive, "interactive", "i", false, "interactive mode")
 
+	setupOldRouteParams(cmd)
 	return cmd
+}
+
+func setupOldRouteParams(cmd *cobra.Command) {
+	flags := cmd.Flags()
+	r := oldRouteOpt.route
+	flags.StringVar(&r.event, "old-"+flagEvent, "", "event type to match")
+	flags.StringVar(&r.pathExact, "old-"+flagPathExact, "", "exact path to match")
+	flags.StringVar(&r.pathRegex, "old-"+flagPathRegex, "", "path regex to match")
+	flags.StringVar(&r.pathPrefix, "old-"+flagPathPrefix, "", "path prefix to match")
+	flags.StringVar(&r.verb, "old-"+flagMethod, "", "HTTP method to match")
+	flags.StringVar(&r.headers, "old-"+flagHeaders, "", "header to match")
+	flags.StringVar(&r.upstream, "old-"+flagUpstream, "", "desitnation upstream")
+	flags.StringVar(&r.function, "old-"+flagFunction, "", "destination function")
+
+	kube := r.kube
+	flags.StringVar(&kube.name, "old-"+flagKubeName, "", "kubernetes service name")
+	flags.StringVar(&kube.namespace, "old-"+flagKubeNamespace, "", "kubernetes service namespace")
+	flags.IntVar(&kube.port, "old-"+flagKubePort, 0, "kubernetes service port")
+
+	// auto complete
+	annotate(cmd.Flag("old-"+flagMethod), "__glooctl_route_http_methods")
+	annotate(cmd.Flag("old-"+flagUpstream), "__glooctl_get_upstreams")
+	annotate(cmd.Flag("old-"+flagFunction), "__glooctl_get_functions")
 }
 
 func runUpdate(sc storage.Interface) {
@@ -57,7 +86,7 @@ func runUpdate(sc storage.Interface) {
 	}
 	fmt.Println("Using virtual service:", v.Name)
 	routes := v.GetRoutes()
-	updated, err := updateRoutes(sc, routes, routeOpt)
+	updated, err := updateRoutes(sc, routes, routeOpt, oldRouteOpt)
 	if err != nil {
 		fmt.Println("Unable to get updated route:", err)
 		os.Exit(1)
@@ -80,7 +109,7 @@ func runUpdate(sc storage.Interface) {
 		}, os.Stdout)
 }
 
-func updateRoutes(sc storage.Interface, routes []*v1.Route, opts *routeOption) ([]*v1.Route, error) {
+func updateRoutes(sc storage.Interface, routes []*v1.Route, opts, oldOpts *routeOption) ([]*v1.Route, error) {
 	if opts.interactive {
 		selection, err := proute.SelectInteractive(routes, false)
 		if err != nil {
@@ -92,17 +121,21 @@ func updateRoutes(sc storage.Interface, routes []*v1.Route, opts *routeOption) (
 		return routes, nil // we have been working with pointers so it has changed the original route
 	}
 
-	route, err := route(opts, sc)
+	newRoute, err := route(opts, sc)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to get route")
+		return nil, errors.Wrap(err, "unable to get new route")
+	}
+	oldRoute, err := route(oldOpts, sc)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get old route")
 	}
 	updated := make([]*v1.Route, len(routes))
 	var matches []*v1.Route
 	for i, r := range routes {
-		if match(route, r) {
+		if match(oldRoute, r) {
 			matches = append(matches, r)
-			route.Extensions = mergeExtensions(route, r)
-			updated[i] = route
+			newRoute.Extensions = mergeExtensions(newRoute, r)
+			updated[i] = newRoute
 			continue
 		}
 		updated[i] = r
