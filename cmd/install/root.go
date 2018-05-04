29812d9a8d07a0a44ad7922dad80ea9f8716bfea
Ashish Shrestha <axhixh@gmail.com>
2018-05-04 11:26:14 -0400

    support installing gloo on kubernetes
    - alias for kubectl apply
    - also updated version

--- cmd/install/root.go
+++ cmd/install/root.go
@@ -9,6 +9,6 @@ func Cmd() *cobra.Command {
 		Use:   "install",
 		Short: "install gloo on different platforms",
 	}
-	cmd.AddCommand(dockerCmd())
+	cmd.AddCommand(dockerCmd(), kubeCmd())
 	return cmd
 }
